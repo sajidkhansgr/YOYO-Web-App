f1d5387524e1b7a85d865b1c8f8b6f76c0283460
Inamullah Handoo <inamullah.javaid.100@gmail.com>
2020-10-01 21:07:46 +0530

    material tab added

--- src/app/hub/hub.module.ts
+++ src/app/hub/hub.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { HubComponent } from './hub.component';
 const routes: Routes = [
-  { path : '', component: HubComponent }
+  { path: '', component: HubComponent }
 ];
 @NgModule({
   declarations: [HubComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatTabsModule
   ]
 })
 export class HubModule { }
