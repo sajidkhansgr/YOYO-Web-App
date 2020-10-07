126887a165325a0237a2cd6b37271a4654373912
Inamullah Handoo <inamullah.javaid.100@gmail.com>
2020-10-07 20:09:15 +0530

    cog dropdown styling

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-user',
@@ -11,22 +11,30 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 })
 export class UserComponent implements OnInit {
   showDoc: boolean = false;
-  visbCols: any[] = [{n:"Role",key:"role"}]
-  hidCols: any[] = [{n:"Property",key:"prop"},{n:"License Type",key:"lic"}]
-  cols: any[] = [{n:"Name",dir:1,key:"name",visb:true},{n:"Latest Activity",dir:0,key:"date",visb:false},
-    {n:"Role",dir:0,key:"role",visb:true},{n:"Property",dir:0,key:"prop",visb:false},
-    {n:"License Type",dir:0,key:"lic",visb:false},{n:"Groups",dir:0,key:"gr",visb:false},{n:"Division",dir:0,key:"div",visb:false},
-    {n:"Experience",dir:0,key:"exp",visb:false}
+  visbCols: any[] = [{ n: "Role", key: "role" }]
+  hidCols: any[] = [{ n: "Property", key: "prop" }, { n: "License Type", key: "lic" }]
+  cols: any[] = [{ n: "Name", dir: 1, key: "name", visb: true }, { n: "Latest Activity", dir: 0, key: "date", visb: false },
+  { n: "Role", dir: 0, key: "role", visb: true }, { n: "Property", dir: 0, key: "prop", visb: false },
+  { n: "License Type", dir: 0, key: "lic", visb: false }, { n: "Groups", dir: 0, key: "gr", visb: false }, { n: "Division", dir: 0, key: "div", visb: false },
+  { n: "Experience", dir: 0, key: "exp", visb: false }
   ]
 
-  data:any[]=[
-    { name: "test",email:"email@email.com",date:"19 Aug 2020",date2:"19 Aug 2020",role:"User" },
-    { name: "tes1t",email:"1email@email.com",date:"19 1Aug 2020",date2:"19 Aug1 2020",role:"U1ser" }
+  data: any[] = [
+    { name: "test", email: "email@email.com", date: "19 Aug 2020", date2: "19 Aug 2020", role: "User" },
+    { name: "tes1t", email: "1email@email.com", date: "19 1Aug 2020", date2: "19 Aug1 2020", role: "U1ser" }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleDropdown = (event: any) => {
+    event.target.nextSibling.classList.toggle('show');
+  }
+
+  closeDropdown = (event: any) => {
+    event.target.parentNode.parentNode.classList.remove('show');
   }
 
   toggleDoc = () => {
@@ -41,17 +49,17 @@ export class UserComponent implements OnInit {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
     }
   }
 
-  showHideCols(col: any, type:string, i:number){
-    if(type==='show'){
+  showHideCols(col: any, type: string, i: number) {
+    if (type === 'show') {
       this.hidCols.splice(i, 1);
       this.visbCols.push(col);
-    }else{
+    } else {
       this.visbCols.splice(i, 1);
       this.hidCols.push(col);
     }
