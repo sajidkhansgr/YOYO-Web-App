fb8e6971fe3bb0932ee8ee6228dcaadda761a3f9
Inamullah Handoo <inamullah.javaid.100@gmail.com>
2020-10-07 23:10:13 +0530

    cog size changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,7 +12,7 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 export class UserComponent implements OnInit {
   showDoc: boolean = false;
   visbCols: any[] = [{ n: "Role", key: "role" }]
-  hidCols: any[] = [{ n: "Property", key: "prop" }, { n: "License Type", key: "lic" }]
+  hidCols: any[] = [{ n: "Property", key: "prop" }, { n: "License Type", key: "lic" }, { n: "License Type", key: "lic" }, { n: "License Type", key: "lic" }, { n: "License Type", key: "lic" }]
   cols: any[] = [{ n: "Name", dir: 1, key: "name", visb: true }, { n: "Latest Activity", dir: 0, key: "date", visb: false },
   { n: "Role", dir: 0, key: "role", visb: true }, { n: "Property", dir: 0, key: "prop", visb: false },
   { n: "License Type", dir: 0, key: "lic", visb: false }, { n: "Groups", dir: 0, key: "gr", visb: false }, { n: "Division", dir: 0, key: "div", visb: false },
@@ -21,7 +21,8 @@ export class UserComponent implements OnInit {
 
   data: any[] = [
     { name: "test", email: "email@email.com", date: "19 Aug 2020", date2: "19 Aug 2020", role: "User" },
-    { name: "tes1t", email: "1email@email.com", date: "19 1Aug 2020", date2: "19 Aug1 2020", role: "U1ser" }
+    { name: "tes1t", email: "1email@email.com", date: "19 1Aug 2020", date2: "19 Aug1 2020", role: "U1ser" },
+    { name: "tes2t", email: "2email@email.com", date: "19 1Aug 2020", date2: "19 Aug1 2020", role: "U2ser" }
   ];
 
   constructor() { }
