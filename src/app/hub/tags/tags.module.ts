ff2ec89bbc3c236ba88ec8f9143b2be561d36040
Inamullah Handoo <inamullah.javaid.100@gmail.com>
2020-10-08 19:49:55 +0530

    changed every bootstrap input/select to material

--- src/app/hub/tags/tags.module.ts
+++ src/app/hub/tags/tags.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TagsComponent } from './tags.component';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [TagsComponent],
   imports: [
     CommonModule,
-    MatFormFieldModule, MatInputModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   exports: [
     TagsComponent
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -7,6 +7,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ClickOutsideModule } from 'ng-click-outside';
+import { MatSelectModule } from '@angular/material/select';
 
 import { UserComponent } from './user.component';
 const routes: Routes = [
@@ -23,7 +24,8 @@ const routes: Routes = [
     MatInputModule,
     MatCheckboxModule,
     DragDropModule,
-    ClickOutsideModule
+    ClickOutsideModule,
+    MatSelectModule
   ]
 })
 export class UserModule { }
