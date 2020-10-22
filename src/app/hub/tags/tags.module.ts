7f44beacd184cc77bad124033cc405775b274f6f
Inamullah Handoo <inamullah.javaid.100@gmail.com>
2020-10-22 18:50:05 +0530

    tags page api's - category getAll, add, getByID, update

--- src/app/hub/tags/tags.module.ts
+++ src/app/hub/tags/tags.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { LoaderModule } from '../../shared/components/loader/loader.module';
 import { ConfirmDialogModule } from '../../shared/components/confirm-dialog/confirm-dialog.module';
 import { TagsComponent } from './tags.component';
+import { TagsService } from './tags.service'
 
 @NgModule({
   declarations: [TagsComponent],
   imports: [
     CommonModule,
+    FormsModule, ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
     ConfirmDialogModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    LoaderModule
   ],
   exports: [
     TagsComponent
-  ]
+  ],
+  providers: [TagsService]
 })
 export class TagsModule { }
