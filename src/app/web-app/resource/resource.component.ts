f4a24fa455859190f223e5a76f0b5dee6d329925
Inamullah Handoo <inamullah.javaid.100@gmail.com>
2020-11-18 15:36:19 +0530

    resource tab navigation added

--- src/app/web-app/resource/resource.component.ts
+++ src/app/web-app/resource/resource.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resource.component.scss']
 })
 export class ResourceComponent implements OnInit {
+  links = [
+    { label: 'Collections', link: '/web-app/resource/collections' },
+    { label: 'Workspaces', link: '/web-app/resource/experiences' },
+    { label: 'My Files', link: '/web-app/resource/my-files' }
+  ];
+  activeLink = this.links[0].label;
 
   constructor() { }
 
--- src/app/web-app/resource/resource.module.ts
+++ src/app/web-app/resource/resource.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+
 import { ResourceComponent } from './resource.component';
 
 const routes: Routes = [
   {
-      path        : '', component: ResourceComponent,
-      children    : [
-        { path : 'collections', loadChildren: () => import('./collection/collection.module').then(m => m.CollectionModule) },
-        { path : 'experiences', loadChildren: () => import('./exp/exp.module').then(m => m.ExpModule) },
-        { path : 'my-files', loadChildren: () => import('./files/files.module').then(m => m.FilesModule) }
-      ]
+    path: '', component: ResourceComponent,
+    children: [
+      { path: 'collections', loadChildren: () => import('./collection/collection.module').then(m => m.CollectionModule) },
+      { path: 'experiences', loadChildren: () => import('./exp/exp.module').then(m => m.ExpModule) },
+      { path: 'my-files', loadChildren: () => import('./files/files.module').then(m => m.FilesModule) }
+    ]
   }
 ];
 
@@ -18,7 +20,8 @@ const routes: Routes = [
   declarations: [ResourceComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatTabsModule
   ]
 })
 export class ResourceModule { }
