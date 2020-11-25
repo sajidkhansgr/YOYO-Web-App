a7ca32fc0053ec87d4dd35978ca33305c942279e
Inamullah Handoo <inamullah.javaid.100@gmail.com>
2020-11-25 13:57:18 +0530

    params in collection fixed

--- src/app/web-app/resource/collection/collection-list/collection-list.component.ts
+++ src/app/web-app/resource/collection/collection-list/collection-list.component.ts
@@ -173,7 +173,7 @@ export class CollectionListComponent implements OnInit {
   // get collection list
   listColct() {
     this.loading = true;
-    this.colctnSrv.colctnList({}).subscribe((data: any) => {
+    this.colctnSrv.colctnList({ pageNo: 0, pageSize: 0 }).subscribe((data: any) => {
       if (data && data.result && Array.isArray(data.result.results) && data.result.results.length > 0) {
         this.colctnArr = data.result.results;
       } else {
