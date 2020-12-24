3a26c05d922dc89faa50c8f9868ff77dadeed3c0
Inamullah Handoo <inamullah.javaid.100@gmail.com>
2020-12-24 20:14:40 +0530

    login carousel fixed

--- src/app/shared/components/auth-slides/auth-slides.module.ts
+++ src/app/shared/components/auth-slides/auth-slides.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap'
+
 import { AuthSlidesComponent } from './auth-slides.component';
 
 @NgModule({
   declarations: [AuthSlidesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbCarouselModule
   ],
   exports: [
     AuthSlidesComponent
