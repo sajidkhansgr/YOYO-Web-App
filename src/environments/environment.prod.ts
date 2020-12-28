f9a6e6b92db5be7ba54601770ec266da8f40e91e
Vikas Kohli <vikas@imobisoft.co.uk>
2020-12-28 11:12:50 +0530

    Prod UAT Build,uat https changes, add env data for prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: ""
+  API_ENDPOINT: "https://appapi.hi-force.com/",
+  SGNL_R: "https://appapi.hi-force.com/notification",
+  CLNT_ID: "8c0cd670273d451cbc9b351b11d22318"
 };
